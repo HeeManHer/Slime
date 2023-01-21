2032cde1e9b10baef8f630a8500022cd374dab8a
HeeManHer <HeeManHer@DESKTOP-QJ9CCQN>
2023-01-21 17:37:21 +0900

    캐릭터 정보

---
+++ src/slime2/CharactersDTO.java
@@ -0,0 +1,33 @@
+package slime2;
+
+public class CharactersDTO {
+
+	private String name;
+	private int hp;
+	private int atk;
+
+	public String getName() {
+		return name;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public int getAtk() {
+		return atk;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public void setAtk(int atk) {
+		this.atk = atk;
+	}
+
+}
