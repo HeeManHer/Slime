ea5a8ccd9365d487773cca0dbc24d119e8937c4c
HeeManHer <HeeManHer@DESKTOP-QJ9CCQN>
2023-01-21 17:38:23 +0900

    기본 슬라임
    일반 공격만 가능

---
+++ src/slime2/Slime.java
@@ -0,0 +1,41 @@
+package slime2;
+
+public class Slime extends CharactersDTO {
+
+	public Slime() {}
+
+	// 생성자
+	public Slime(String name) {
+		setName(name);
+		setHp(50);
+		setAtk(10);
+	}
+	
+	public Slime getInstance() {
+		return new Slime(getName());
+	}
+
+	public void attack(Human hunter) {
+		attack(hunter, getAtk());
+	}
+
+	// 공격
+	public void attack(Human hunter, int damage) {
+		if (getHp() > 0) {
+			hunter.setHp(hunter.getHp() - damage);
+
+			if (hunter.getHp() < 1) {
+				System.out.println("Game Over");
+				return;
+			}
+
+			if (damage < 30) {
+				System.out.println(getName() + "의 공격. " + hunter.getName() + "의 체력은 " + hunter.getHp() + "이다.");
+			} else {
+				System.out.println(getName() + "의 강한 공격. " + hunter.getName() + "의 체력은 " + hunter.getHp() + "이다.");
+			}
+
+		}
+
+	}
+}
