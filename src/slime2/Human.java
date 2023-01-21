4bd160c3f72aa41ff8e0eebe1754c38f3bf10d0a
HeeManHer <HeeManHer@DESKTOP-QJ9CCQN>
2023-01-21 17:37:48 +0900

    헌터

---
+++ src/slime2/Human.java
@@ -0,0 +1,38 @@
+package slime2;
+
+public class Human extends CharactersDTO {
+
+	public Human(String name) {
+		setName(name);
+		setHp(100);
+		setAtk(10);
+	}
+
+	public void Info() {
+		System.out.println("직업 : " + getName());
+		System.out.println("체력 : " + getHp());
+		System.out.println("공격력 : " + getAtk());
+	}
+
+	public void Heal() {
+		if (getHp() >= 80) {
+			System.out.println("체력은 충분하군.");
+		} else {
+			System.out.println("체력을 회복합니다.");
+			setHp(100);
+		}
+	}
+
+	public void attack(Slime s) {
+
+		s.setHp(s.getHp() - getAtk());
+
+		if (s.getHp() < 1) {
+			System.out.println(s.getName() + "는 사망했다.");
+		} else {
+			System.out.println(s.getName() + "을 공격했다.");
+			System.out.println("현재 " + s.getName() + "의 체력은 " + s.getHp() + "이다.");
+		}
+
+	}
+}
