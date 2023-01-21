bfe8b2d507e7cc980d9a6c66603e2939b1500057
HeeManHer <HeeManHer@DESKTOP-QJ9CCQN>
2023-01-21 17:38:45 +0900

    블루 슬라임
    공격력은 약하지만 회복 가능

---
+++ src/slime2/BlueSlime.java
@@ -0,0 +1,26 @@
+package slime2;
+
+public class BlueSlime extends Slime {
+
+	public BlueSlime(String name) {
+		setName(name);
+		setHp(30);
+		setAtk(5);
+	}
+	
+	@Override
+	public Slime getInstance() {
+		return new BlueSlime(getName());
+	}
+	
+	public void heal() {
+		if (getHp() > 0) {
+			setHp(getHp() + 10);
+			if (getHp() > 80) {
+				setHp(80);
+			}
+
+			System.out.println(getName() + "는 치료를 했다. 그의 체력은" + getHp() + "이다.");
+		}
+	}
+}
